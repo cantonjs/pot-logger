a5306e0067733b6faa50cdc15778170fd2d18cb7
Webb Peng <webb.png@gmail.com>
2017-07-30 22:34:21 +0800

    fix testing in ci

--- test/utils.js
+++ test/utils.js
@@ -1,6 +1,6 @@
 
 import { resolve } from 'path';
-import { readFileSync, writeFileSync } from 'fs-extra';
+import { readFileSync, outputFileSync } from 'fs-extra';
 import SandboxedModule from 'sandboxed-module';
 import { transformFileSync } from 'babel-core';
 import { parse } from 'json5';
@@ -8,9 +8,9 @@ import { parse } from 'json5';
 export function build() {
 	const babelOptions = parse(readFileSync(resolve('.babelrc'), 'utf8'));
 	const { code } = transformFileSync(resolve('src/index.js'), babelOptions);
-	writeFileSync(resolve('lib/index.js'), code, 'utf8');
+	outputFileSync(resolve('lib/index.js'), code, 'utf8');
 }
 
 export function requireSandbox(globals) {
-	return SandboxedModule.require('../lib', { globals });
+	return SandboxedModule.require(resolve('lib/index.js'), { globals });
 }
